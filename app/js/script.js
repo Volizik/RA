8f96d7807b46cba23dd663e65462880c00bb6cb0
Kizilov Yura <kizilov.yura@gmail.com>
2017-07-17 16:45:19 +0300

    add progressbar

--- app/js/script.js
+++ app/js/script.js
@@ -146,4 +146,30 @@ $(function () {
         });
 
     }
+
+    $('.img').circleProgress({
+        value: 1,
+        size: 245,
+        thickness: 38,
+        startAngle: 4.74,
+        emptyFill: "#fff",
+        animation: {
+            duration: 2000
+        },
+        fill: {
+            color: "transparent"
+        }
+    });
+
+    $('.js-counter').each(function () {
+        $(this).prop('Counter',0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 2000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
 });
