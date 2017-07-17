c04310ab2da48cd333deab3de27ea19d2868a98e
Kizilov Yura <kizilov.yura@gmail.com>
2017-07-17 13:18:21 +0300

    add list-sign

--- app/js/script.js
+++ app/js/script.js
@@ -3,15 +3,18 @@ $(function () {
     $(document).on('click', '.menu__body__list>a', function () {
         var submenu = $(this).parent().find('.submenu');//находим элемент в ДОМ
         var p = $(this).find('p');
+        $('.menu__body__list a').find('.list-sign').html('+');
         if ($(this).hasClass('show-submenu')) {//если у элемента по которому мы кликаем есть класс
             $(this).removeClass('show-submenu');//то удаляем этот класс
             submenu.hide();//скрываем сабменю
+            $('.menu__body__list a').find('.list-sign').html('+');
         }
         else {
             $('.menu__body__list a').removeClass('show-submenu');//удаляем класс у всех элементов
             $('.submenu').hide();//скрываем все сабменю
             $(this).addClass('show-submenu');//добавляем класс тому элементу по которому кликаем
             submenu.show();//показать табменю того элемента покоторому кликаем
+            $(this).find('.list-sign').html('-');
         }
     });
 
