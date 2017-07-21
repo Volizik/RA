71be48a5dc0f179eef938ecf700dd4ddb04aac33
Kizilov Yura <kizilov.yura@gmail.com>
2017-07-21 12:16:54 +0300

    add fix for users desctop

--- app/js/script.js
+++ app/js/script.js
@@ -175,54 +175,88 @@ $(function () {
 
     //charts
 
-    google.charts.load('current', {'packages':['bar']});
-    google.charts.setOnLoadCallback(drawChart);
-    function drawChart() {
+    // Load the Visualization API and the controls package.
+    google.charts.load('current', {'packages':['corechart', 'controls']});
+
+    // Set a callback to run when the Google Visualization API is loaded.
+    google.charts.setOnLoadCallback(drawDashboard);
+
+    // Callback that creates and populates a data table,
+    // instantiates a dashboard, a range slider and a pie chart,
+    // passes in the data and draws it.
+    function drawDashboard() {
+
+        // Create our data table.
         var data = google.visualization.arrayToDataTable([
-            [' ', 'Посты', 'Реакции'],
-            ['1', 1000, 400],
-            ['2', 1170, 460],
-            ['3', 660, 1120],
-            ['4', 1030, 540],
-            ['5', 1000, 400],
-            ['6', 1170, 460],
-            ['7', 660, 1120],
-            ['8', 1000, 400],
-            ['9', 1170, 460],
-            ['10', 660, 1120],
-            ['11', 1000, 400],
-            ['12', 1170, 460],
-            ['13', 660, 1120],
-            ['14', 1000, 400],
-            ['15', 1170, 460],
-            ['16', 660, 1120],
-            ['17', 1030, 540],
-            ['18', 1000, 400],
-            ['19', 1170, 460],
-            ['20', 660, 1120],
-            ['21', 1000, 400],
-            ['22', 1170, 460],
-            ['23', 660, 1120],
-            ['24', 1000, 400],
-            ['25', 1170, 460],
-            ['26', 660, 1120],
+            ['Name', ' ', 'Посты', 'Реакции'],
+            [' ', 1, 1000, 400],
+            [' ', 2, 1170, 460],
+            [' ', 3, 660, 1120],
+            [' ', 4, 1030, 540],
+            [' ', 5, 1000, 400],
+            [' ', 6, 1170, 460],
+            [' ', 7, 660, 1120],
+            [' ', 8, 1000, 400],
+            [' ', 9, 1170, 460],
+            [' ', 10, 660, 1120],
+            [' ', 11, 1000, 400],
+            [' ', 12, 1170, 460],
+            [' ', 13, 660, 1120],
+            [' ', 14, 1000, 400],
+            [' ', 15, 1170, 460],
+            [' ', 16, 660, 1120],
+            [' ', 17, 1030, 540],
+            [' ', 18, 1000, 400],
+            [' ', 19, 1170, 460],
+            [' ', 20, 660, 1120],
+            [' ', 21, 1000, 400],
+            [' ', 22, 1170, 460],
+            [' ', 23, 660, 1120],
+            [' ', 24, 1000, 400],
+            [' ', 25, 1170, 460],
+            [' ', 26, 660, 1120],
         ]);
 
-        var options = {
-            legend: { position: 'left'},
-            chart: {
-                title: '',
-                subtitle: '',
+        // Create a dashboard.
+        var dashboard = new google.visualization.Dashboard(
+            document.getElementById('dashboard_div'));
+
+        // Create a range slider, passing some options
+        var donutRangeSlider = new google.visualization.ControlWrapper({
+            'controlType': 'NumberRangeFilter',
+            'containerId': 'filter_div',
+            'options': {
+                'filterColumnLabel': ' ',
+                'minValue': 1,
+                'maxValue': 26,
+                'width': 500
             },
-            bars: 'vertical',
-            vAxis: {format: 'decimal'},
-            height: 350,
-            width: 1300,
-            colors: ['#ff5057', '#2196f3']
-        };
+            // Explicitly positions the thumbs at position 3 and 8,
+            // out of the possible range of 1 to 10.
+            'state': {'lowValue': 1, 'highValue': 26}
+        });
 
-        var chart = new google.charts.Bar(document.getElementById('chart_div'));
+        // Create a pie chart, passing some options
+        var columnChart = new google.visualization.ChartWrapper({
+            'chartType': 'ColumnChart',
+            'containerId': 'chart_div',
+            'options': {
+                'width': 1100,
+                'height': 500,
+            },
+            // The pie chart will use the columns 'Name' and 'Donuts eaten'
+            // out of all the available ones.
+            'view': {'columns': [1, 2, 3]}
+        });
 
-        chart.draw(data, google.charts.Bar.convertOptions(options));
+
+        // Establish dependencies, declaring that 'filter' drives 'pieChart',
+        // so that the pie chart will only display entries that are let through
+        // given the chosen slider range.
+        dashboard.bind(donutRangeSlider, columnChart);
+
+        // Draw the dashboard.
+        dashboard.draw(data);
     }
 });
+
